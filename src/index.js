2008dfbcc96947eaf6583e606cc2aa9cda37aedb
Carlos Pires <extraterra1@gmail.com>
2023-10-06 16:48:50 +0100

    feat: build grid automatically

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './style.css';
-console.log('hello world!');
+
+const buildBoardGrid = (parent) => {
+  for (let i = 0; i < 100; i++) {
+    const elementToAppend = document.createElement('div');
+    elementToAppend.classList.add('grid-item');
+    parent.append(elementToAppend);
+  }
+};
+
+const userGrid = document.querySelector('.user-grid .board-grid');
+const cpuGrid = document.querySelector('.cpu-grid .board-grid');
+
+buildBoardGrid(userGrid);
+buildBoardGrid(cpuGrid);
