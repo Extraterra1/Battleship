bcd9689cd0ac00fe1be0d395cd4f0aff6a0f539d
Carlos Pires <extraterra1@gmail.com>
2023-10-07 11:36:01 +0100

    feat: add isValidMove method

--- src/index.js
+++ src/index.js
@@ -20,8 +20,9 @@ const gameboard = new Gameboard();
 const ship = new Ship(2);
 
 gameboard.addShip(ship, [0, 0]);
-gameboard.receiveAttack([0, 0]);
-gameboard.receiveAttack([1, 0]);
+console.log(gameboard.receiveAttack([0, 0]));
+console.log(gameboard.receiveAttack([1, 0]));
+console.log(gameboard.receiveAttack([3, 0]));
 
 console.log(gameboard.board);
 console.log(gameboard.ships);
--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -6,18 +6,35 @@ export default class Gameboard {
 
   addShip(ship, coords) {
     const [x, y] = coords;
-    this.ships.push(ship);
+    const positions = [];
+
     for (let i = x; i < ship.length + x; i++) {
       this.board[i][y] = { ...this.board[i][y], ship };
+      positions.push([i, y]);
     }
+
+    ship.positions = positions;
+    this.ships.push(ship);
   }
 
   receiveAttack(coords) {
+    if (!this.isValidMove(coords)) throw new Error('Invalid Move');
     const [x, y] = coords;
     const cell = this.board[x][y];
+    let message = `attacked [${x},${y}]`;
 
-    if (cell.ship) cell.ship.hit();
-    if (cell.ship.isSunk()) console.log('ship has been sunk!');
+    if (cell.ship) {
+      const ship = this.ships.find((s) => s === cell.ship);
+      ship.hit();
+      ship.isSunk() ? (message += ' ship has been sunk!') : (message += ' ship found!');
+    }
+
     this.board[x][y] = { ...cell, hasBeenHit: true };
+    return message;
+  }
+
+  isValidMove(coords) {
+    const [x, y] = coords;
+    return x >= 0 && x <= 9 && y >= 0 && y <= 9 && !this.board[x][y].hasBeenHit;
   }
 }
