c3ca1f1846359aaeea108c757a1636f9d53f8b72
Carlos Pires <extraterra1@gmail.com>
2023-10-08 19:13:57 +0100

    feat: add coord data key to each cell

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import Gameboard from './modules/Gameboard';
 import './style.css';
 
 const buildBoardGrid = (parent) => {
+  let row = 0;
+  let column = 0;
   for (let i = 0; i < 100; i++) {
     const elementToAppend = document.createElement('div');
     elementToAppend.classList.add('grid-item');
+    elementToAppend.setAttribute('data-coords', `${row},${column}`);
     parent.append(elementToAppend);
+
+    if (++column === 10) {
+      row++;
+      column = 0;
+    }
   }
 };
 
